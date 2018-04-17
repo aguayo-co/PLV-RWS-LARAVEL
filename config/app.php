66c272ab168869636e49bf9145d6a048be033535
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2018-04-17 18:06:38 -0500

    [Laravel] Force HTTPS

--- config/app.php
+++ config/app.php
@@ -150,6 +150,7 @@ return [
         /*
          * Package Service Providers...
          */
+        \Ecrmnn\LaravelHttps\Providers\ServiceProvider::class,
 
         /*
          * Application Service Providers...
