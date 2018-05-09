25ebd942034180b11e7920db243b32685d3ca209
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2018-05-09 14:16:36 -0500

    [Ratings] Single rating route is public now.

--- routes/api/ratings.php
+++ routes/api/ratings.php
@@ -1,10 +1,10 @@
 <?php
 Route::get('', 'RatingController@index')->name('ratings');
+Route::get('{rating}', 'RatingController@show')->name('rating.get')->where('rating', ID_REGEX);
 
 # Auth routes.
 # Only authenticated requests here.
 Route::middleware('auth:api')->group(function () {
-    Route::get('{rating}', 'RatingController@show')->name('rating.get')->where('rating', ID_REGEX);
     Route::patch('{rating}', 'RatingController@update')->name('rating.update')->where('rating', ID_REGEX);
     Route::delete('{rating}', 'RatingController@delete')->name('rating.delete')->where('rating', ID_REGEX);
 });
