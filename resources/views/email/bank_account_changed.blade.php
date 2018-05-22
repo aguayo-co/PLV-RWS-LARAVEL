c365df3e5eb6b09633a6c955614b52beed0e8055
Geraldine Díaz <geraldine.diaz@aguayopublicidad.com>
2018-05-22 18:10:08 -0500

    [Emails] - Titulo en mayusculas (estilos) bank acoount changed y cambio de subject

--- resources/views/email/bank_account_changed.blade.php
+++ resources/views/email/bank_account_changed.blade.php
@@ -117,11 +117,13 @@
 
     .title {
       display: inline-block;
+      margin-top: 40px;
       font-size: 30px;
       font-weight: bold;
-      border-bottom: 1px solid #979797;
       font-family: 'Montserrat', Arial, Helvetica, sans-serif;
       line-height: 1.4;
+      text-transform: uppercase;
+      border-bottom: 1px solid #979797;
     }
 
     .highlight {
@@ -213,10 +215,10 @@
                 <td class="cell" align="center" style="padding-right:0;padding-left:0;padding-top:0;padding-bottom:0;">
                   <table class="table__small" style="max-width:460px;border-collapse:collapse;-ms-text-size-adjust:100%;-webkit-text-size-adjust:100%;">
                     <tr class="row" style="width:100%;">
-                      <td class="cell spacing_title center" style="padding-top:40px;padding-bottom:10px;padding-right:20px;padding-left:20px;text-align:center;">
-                        <p class="title" style="margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;display:inline-block;font-size:30px;font-weight:bold;border-bottom-width:1px;border-bottom-style:solid;border-bottom-color:#979797;font-family:'Montserrat', Arial, Helvetica, sans-serif;line-height:1.4;">Hola
+                      <td class="cell center" style="padding-right:0;padding-left:0;padding-top:0;padding-bottom:0;text-align:center;">
+                        <h1 class="title" style="display:inline-block;margin-top:40px;font-size:30px;font-weight:bold;font-family:'Montserrat', arial;line-height:1.4;text-transform:uppercase;border-bottom-width:1px;border-bottom-style:solid;border-bottom-color:#979797;">Hola
                           <span class="highlight" style="color:#f65a66;">{{ $user->first_name }}</span>
-                        </p>
+                        </h1>
                       </td>
                     </tr>
                     <tr class="row" style="width:100%;">
