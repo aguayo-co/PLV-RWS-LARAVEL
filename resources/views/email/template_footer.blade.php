a6e363c221162d39af480a1c1273006b32556324
Geraldine Díaz <geraldine.diaz@aguayopublicidad.com>
2018-05-24 12:41:07 -0500

    [Emails] - Creacion de bloques de template: header y footer para todos los mailings, integracion de bloques de template y ajustes en estilos de footer

---
+++ resources/views/email/template_footer.blade.php
@@ -0,0 +1,29 @@
+<tr class="row" style="width:100%;">
+  <td class="cell cell_footer center" center style="padding-right:0;padding-left:0;padding-top:0;padding-bottom:0;text-align:center;background-color:#F6F6F6;">
+    <table width="600" cellpadding="0" cellspacing="0" border="0">
+      <tbody>
+        <tr>
+          <td class="center" center style="padding-top:20px;padding-bottom:20px;padding-right:0;padding-left:0;font-size:16px;font-weight:bold;font-family:'Montserrat', Arial, Helvetica, sans-serif;line-height:1.4;text-align:center;">Síguenos</td>
+        </tr>
+        <tr>
+          <td class="center" style="text-align:center;">
+            <a href="https://www.instagram.com/prilovchile/?hl=es-la" class="footer__link" title="Síguenos en Instragram" style="display:inline-block;padding-top:0;padding-bottom:0;padding-right:16px;padding-left:16px;">
+              <img src="{{ env('APP_FRONT_URL') }}static/img/mailing/instagram.jpg" alt="Instragram" class="img">
+            </a>
+            <a href="https://twitter.com/prilovchile?lang=es" class="footer__link" title="Síguenos en Twitter" style="display:inline-block;padding-top:0;padding-bottom:0;padding-right:16px;padding-left:16px;">
+              <img src="{{ env('APP_FRONT_URL') }}static/img/mailing/twitter.jpg" alt="Twitter" class="img">
+            </a>
+            <a href="https://www.facebook.com/prilovchile" class="footer__link" title="Síguenos en Facebook" style="display:inline-block;padding-top:0;padding-bottom:0;padding-right:16px;padding-left:16px;">
+              <img src="{{ env('APP_FRONT_URL') }}static/img/mailing/facebook.jpg" alt="Facebook" class="img">
+            </a>
+          </td>
+        </tr>
+        <tr>
+          <td class="footer__txt center" center style="padding-top:20px;padding-bottom:20px;padding-right:20px;padding-left:20px;margin-top:0;margin-bottom:0;margin-right:0;margin-left:0;font-size:14px;font-family:'Montserrat', Arial, Helvetica, sans-serif;line-height:1.4;text-align:center;">
+            Copyright © 2017 prilov.com. Todos los derechos reservados.
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </td>
+</tr>
