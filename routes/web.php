db949d488f15d5c4a9bc100f039a999103975d8e
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2018-07-17 10:57:17 -0500

    [Helpers] Domain validation helpers

--- routes/web.php
+++ routes/web.php
@@ -10,3 +10,24 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+// Helper for domain validations:
+// Set an ENV variable named DOMAIN_VALIDATION.
+// Each path should be separated by 3 underscores,
+// and if the response needs some content,
+// put it after 3 dashes for each path
+//   Example: pathA___pathB---content___pathC---content C
+//
+// example:
+// random_0123456789.html---some-content-that-should-be-in-the-file___random_0123456789
+
+$validations = env('DOMAIN_VALIDATION');
+if ($validations) {
+    $lines = explode('___', $validations);
+    foreach ($lines as $line) {
+        $data = explode('---', $line);
+        Route::get($data[0], function () use ($data) {
+            return array_get($data, 1);
+        });
+    }
+}
