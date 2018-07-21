74b487a9aed3c5595d1aeee07c92b70dcc5f1afb
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2018-07-21 15:46:39 -0500

    [Payment] Receipt can be uploaded on rejected transfers

--- app/Http/Controllers/Order/OrderControllerRules.php
+++ app/Http/Controllers/Order/OrderControllerRules.php
@@ -143,7 +143,7 @@ trait OrderControllerRules
             if ($payment->gateway !== 'Transfer') {
                 return $fail(__('El pago no es de tipo Transferencia.'));
             }
-            if ($payment->status !== Payment::STATUS_PENDING && $payment->status !== Payment::STATUS_PROCESSING) {
+            if ($payment->status === Payment::STATUS_SUCCESS || $payment->status === Payment::STATUS_CANCELED) {
                 return $fail(__('El pago ya no acepta recibos.'));
             }
         };
