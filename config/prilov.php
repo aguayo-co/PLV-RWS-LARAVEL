26fb4b23171489e0f281184fc45896e68593008f
Federico Jaramillo Martínez <federicojaramillom@gmail.com>
2018-08-01 22:01:30 -0500

    [Prilov] Change configuration values

--- config/prilov.php
+++ config/prilov.php
@@ -3,11 +3,11 @@
 return [
     'sales' => [
         'days_shipping_to_delivered' => env('PRILOV_DAYS_SHIPPING_TO_DELIVERED', 3),
-        'days_delivered_to_completed' => env('PRILOV_DAYS_DELIVERED_TO_COMPLETED', 2),
+        'days_delivered_to_completed' => env('PRILOV_DAYS_DELIVERED_TO_COMPLETED', 5),
         'days_to_publish_ratings' => env('PRILOV_DAYS_TO_PUBLISH_RATINGS', 2),
     ],
     'sale_returns' => [
-        'days_created_to_canceled' => env('PRILOV_DAYS_CREATED_TO_CANCELED', 2),
+        'days_created_to_canceled' => env('PRILOV_DAYS_CREATED_TO_CANCELED', 5),
     ],
     'payments' => [
         'minutes_until_canceled' => env('PRILOV_MINUTES_UNTIL_CANCELED', 60),
